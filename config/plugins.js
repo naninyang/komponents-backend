03ef1ba5d3a5efb882313141bd9a8935ae3336d5
Chloe <gtalk.with.nani@gmail.com>
2023-12-07 19:01:22 +0900

    email

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,19 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-plugin-mandrill",
+      providerOptions: {
+        apiKey: process.env.MANDRILL_API_KEY,
+      },
+      settings: {
+        defaultFrom: "1157iamari@gmail.com",
+        defaultName: "Chloe",
+        defaultReplyTo: "1157iamari@gmail.com",
+        defaultHtml: "Test",
+        defaultText: "Test",
+      },
+    },
+  },
   upload: {
     provider: "aws-s3",
     providerOptions: {
